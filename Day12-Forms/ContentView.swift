06e974dbc655d5afa769d2abc2bfbc54195fd65d
BeatrizECardozoLopez <beatrizecardozolopez@gmail.com>
2024-08-13 09:20:20 -0400

    Initial Commit

---
+++ Day12-Forms/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Day12-Forms
+//
+//  Created by Beatriz Cardozo on 13/8/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Day12-Forms/Day12_FormsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Day12_FormsApp.swift
+//  Day12-Forms
+//
+//  Created by Beatriz Cardozo on 13/8/24.
+//
+
+import SwiftUI
+
+@main
+struct Day12_FormsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
